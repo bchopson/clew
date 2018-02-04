26db019eaa28ff64d2b00876ab8f96deee93e112
Ben Chopson <bjamin513@gmail.com>
2018-02-04 16:11:13 +0000

    Add test for a whole game from Gettysburg paper

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -142,6 +142,301 @@ class TestEngine():
             [-36], [-34], [-32], [-31], [-30], [-29], [-27], [-26], [-25],
             [-24], [-23], [-14], [-12], [-7], [-1], [22], [28], [33], [35],]
 
+    def test_whole_game(self):
+        game = Game(
+            primary_player='Miss Scarlett',
+            players=[
+                Player(
+                    name='Miss Scarlett',
+                    card_count=3,
+                    people=['Mrs. White'],
+                    rooms=['library', 'study']),
+                Player(name='Col. Mustard', card_count=3),
+                Player(name='Mrs. White', card_count=3),
+                Player(name='Mr. Green', card_count=3),
+                Player(name='Mrs. Peacock', card_count=3),
+                Player(name='Prof. Plum', card_count=3),
+            ])
+        engine = Engine(game)
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Miss Scarlett',
+                person='Miss Scarlett',
+                weapon='rope',
+                room='lounge',
+                answerer='Col. Mustard',
+                was_card_shown=True,
+                card_shown='Miss Scarlett',
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Col. Mustard',
+                person='Mrs. Peacock',
+                weapon='lead pipe',
+                room='dining room',
+                answerer='Mrs. Peacock',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. White',
+                person='Col. Mustard',
+                weapon='revolver',
+                room='ballroom',
+                answerer='Mrs. Peacock',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mr. Green',
+                person='Mrs. White',
+                weapon='dagger',
+                room='ballroom',
+                answerer='Prof. Plum',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. Peacock',
+                person='Mr. Green',
+                weapon='candlestick',
+                room='dining room',
+                answerer='Mrs. White',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Miss Scarlett',
+                person='Prof. Plum',
+                weapon='rope',
+                room='ballroom',
+                answerer='Col. Mustard',
+                was_card_shown=True,
+                card_shown='Prof. Plum',
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Col. Mustard',
+                person='Mrs. Peacock',
+                weapon='rope',
+                room='ballroom',
+                answerer='Mrs. White',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. White',
+                person='Col. Mustard',
+                weapon='candlestick',
+                room='study',
+                answerer='Mr. Green',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mr. Green',
+                person='Mrs. Peacock',
+                weapon='dagger',
+                room='dining room',
+                answerer='Mrs. Peacock',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. Peacock',
+                person='Col. Mustard',
+                weapon='lead pipe',
+                room='dining room',
+                answerer='Prof. Plum',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Prof. Plum',
+                person='Mr. Green',
+                weapon='dagger',
+                room='conservatory',
+                answerer='Mrs. White',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Miss Scarlett',
+                person='Mrs. Peacock',
+                weapon='dagger',
+                room='lounge',
+                answerer='Col. Mustard',
+                was_card_shown=True,
+                card_shown='lounge',
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Col. Mustard',
+                person='Mrs. Peacock',
+                weapon='dagger',
+                room='dining room',
+                answerer='Mrs. White',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. White',
+                person='Mrs. Peacock',
+                weapon='wrench',
+                room='hall',
+                answerer='Mr. Green',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mr. Green',
+                person='Mrs. White',
+                weapon='lead pipe',
+                room='conservatory',
+                answerer='Prof. Plum',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. Peacock',
+                person='Miss Scarlett',
+                weapon='lead pipe',
+                room='hall',
+                answerer='Col. Mustard',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Prof. Plum',
+                person='Mrs. Peacock',
+                weapon='lead pipe',
+                room='ballroom',
+                was_card_shown=False,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Miss Scarlett',
+                person='Mrs. White',
+                weapon='lead pipe',
+                room='hall',
+                answerer='Mrs. Peacock',
+                was_card_shown=True,
+                card_shown='hall',
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. White',
+                person='Mrs. Peacock',
+                weapon='lead pipe',
+                room='hall',
+                answerer='Mrs. Peacock',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. Peacock',
+                person='Mrs. Peacock',
+                weapon='lead pipe',
+                room='hall',
+                was_card_shown=False,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Miss Scarlett',
+                person='Mr. Green',
+                weapon='lead pipe',
+                room='study',
+                answerer='Mrs. White',
+                was_card_shown=True,
+                card_shown='Mr. Green',
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Col. Mustard',
+                person='Mrs. Peacock',
+                weapon='lead pipe',
+                room='ballroom',
+                answerer='Prof. Plum',
+                was_card_shown=True,
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. White',
+                person='Mrs. Peacock',
+                weapon='lead pipe',
+                room='study',
+                answerer='Miss Scarlett',
+                was_card_shown=True,
+                card_shown='study',
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mr. Green',
+                person='Mrs. White',
+                weapon='lead pipe',
+                room='study',
+                answerer='Miss Scarlett',
+                was_card_shown=True,
+                card_shown='Mrs. White',
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Mrs. Peacock',
+                person='Mrs. White',
+                weapon='lead pipe',
+                room='study',
+                answerer='Miss Scarlett',
+                was_card_shown=True,
+                card_shown='Mrs. White',
+            ))
+        engine.suggest(
+            Guess(
+                index=len(engine.game.guesses),
+                guesser='Prof. Plum',
+                person='Mrs. Peacock',
+                weapon='lead pipe',
+                room='kitchen',
+                answerer='Mr. Green',
+                was_card_shown=True,
+            ))
+        engine.accuse(
+            Accusation(
+                index=len(engine.game.accusations),
+                accuser='Miss Scarlett',
+                person='Mrs. Peacock',
+                weapon='lead pipe',
+                room='billiard room',
+                is_correct=True,
+            ))
+        for entry in engine.notebook:
+            assert entry['has_card'] != Engine.UNKNOWN
+
+
     def create_game_sparse_players(self):
         game = Game()
         game.primary_player = PEOPLE[0]
